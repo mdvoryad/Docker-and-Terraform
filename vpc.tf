fecbeaf123d28f920f93de29477cab987040a643
mdvoryad <99253433+mdvoryad@users.noreply.github.com>
2023-08-13 18:30:00 +0200

    Create vpc.tf

---
+++ vpc.tf
@@ -0,0 +1,43 @@
+provider "aws" {
+  region = "eu-west-2"
+}
+
+resource "aws_vpc" "vpc_team1" {
+  cidr_block           = "10.0.0.0/28"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "Team 1 (Gateway Team)"
+  }
+}
+
+resource "aws_vpc" "vpc_team2" {
+  cidr_block           = "10.0.0.16/28"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "Team 2 (Order Team)"
+  }
+}
+
+resource "aws_vpc" "vpc_team3" {
+  cidr_block           = "10.0.0.32/28"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "Team 3 (External Team)"
+  }
+}
+
+resource "aws_vpc" "vpc_team4" {
+  cidr_block           = "10.0.0.48/28"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "Team 4 (Data Team)"
+  }
+}
